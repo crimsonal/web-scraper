a4f42103a2d28d68ea018aa53152e52ce90d4d6e
christopher-artis <carti002@odu.edu>
2026-01-14 18:52:55 -0500

    extracts title from page and prints it to console using the CLI

--- scrape.py
+++ scrape.py
@@ -1,1 +1,12 @@
 from bs4 import BeautifulSoup
+import requests
+import sys
+
+def extractFromTag(tag, tagName):
+    extraction = tag.split(tagName)
+    print(extraction)
+
+for arg in sys.argv[1:]:
+    response = requests.get(arg)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.title.text)
