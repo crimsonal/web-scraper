3159c8acf8cee8b382bd5c70a3a148a6f717a65b
christopher-artis <carti002@odu.edu>
2026-01-14 18:56:14 -0500

    remove unneeded function

--- scrape.py
+++ scrape.py
@@ -2,9 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
-def extractFromTag(tag, tagName):
-    extraction = tag.split(tagName)
-    print(extraction)
 
 for arg in sys.argv[1:]:
     response = requests.get(arg)
