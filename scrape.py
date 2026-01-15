4be8fbee793dfb450275cc14c60fd33cc693388b
christopher-artis <carti002@odu.edu>
2026-01-14 21:15:51 -0500

    extracts domain and description from url

--- scrape.py
+++ scrape.py
@@ -2,8 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
+def parseTextForDomain(url):
+    return url.split("/")[2]
 
 for arg in sys.argv[1:]:
     response = requests.get(arg)
+    domain = parseTextForDomain(response.url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup.title.text)
+    title = soup.title.text 
+    description = soup.find("meta", {"name":"description"})['content']
+    
+    print(f"Title: {title}\nDescription: {description}\nDomain: {domain}")
